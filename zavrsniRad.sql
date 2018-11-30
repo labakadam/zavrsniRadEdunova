36e53dec76c1b51bd324d6126179c8148d922e3b
Adam Labak <labakadam1993@gmail.com>
2018-11-30 08:29:40 +0100

    Uređen era dijagram

--- zavrsniRad.sql
+++ zavrsniRad.sql
@@ -1,24 +1,24 @@
 drop database if exists zavrsniRad;
 create database zavrsniRad;
 use zavrsniRad;
-create table safeFlyEu(
+create table avioKompanija(
 				id 					int not null primary key auto_increment,
-				informacija 		varchar(50) not null,
-				avioKompanija 		int not null,
-				let 				int not null,
-				cijena 				decimal not null,
-				vrijeme 			datetime not null,
-				grad  				varchar(50)
+				let 				varchar(50) not null,
+				avion 				varchar(50) not null,
+				zaposlenik			int not null,
+				oib 				char(11) not null,
+				iban 				char(21) not null,
+				osiguranje          int not null
 );
 create table zaposlenik(
 				id 					int not null primary key auto_increment,
+				avioKompanija       int not null,
 				ime 				varchar(50) not null,
 				prezime 			varchar(50) not null,
 				email 				varchar(50) not null,
-				mobitel 			int,
-				brojUgovora 		int not null,
-				oib 				char(11),
-				avioKompanija       int not null
+				mobitel 			int not null,
+				brojUgovora 		int,
+				oib 				char(11)
 );
 create table korisnik(
 				id 					int not null primary key auto_increment,
@@ -29,15 +29,6 @@ create table korisnik(
 				brojMobitela 		int,
 				osiguranje          int not null
 );
-create table avioKompanija(
-				id 					int not null primary key auto_increment,
-				let 				int not null,
-				avion 				int not null,
-				zaposlenik			int not null,
-				oib 				char(11) not null,
-				iban 				char(21),
-				osiguranje          int not null
-);
 create table osiguranje(
 				id 					int not null primary key auto_increment,
 				naziv		 		varchar(50) not null,
@@ -45,11 +36,18 @@ create table osiguranje(
 				iban				char(21) not null,
 				oib 				char(11)
 );
+create table korisnik_avioKompanija(
+				korisnik 			int not null,
+				avioKompanija	 	int not null,
+				oznakaSjedala       varchar(50) not null,
+				hrana               boolean,
+				pice                boolean,
+				prtljaga            boolean
+);
 
 	alter table korisnik 			add foreign key (osiguranje)		references osiguranje(id);
-	alter table korisnik 			add foreign key (id)				references safeFlyEu(id);
-	alter table avioKompanija 		add foreign key (zaposlenik) 		references zaposlenik(id);
+	alter table twain 			    add foreign key (korisnik)		    references korisnik(id);
+	alter table twain        		add foreign key (avioKompanija) 	references avioKompanija(id);
 	alter table avioKompanija 		add foreign key (osiguranje) 		references osiguranje(id);
-	alter table zaposlenik 			add foreign key (avioKompanija)   	references safeFlyEu(id);
-
+	alter table avioKompanija 		add foreign key (zaposlenik)   	    references zaposlenik(id);
 
